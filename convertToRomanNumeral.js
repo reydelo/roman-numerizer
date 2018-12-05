df667daa83be9f585f03ce86859bd8a233f94381
Reyna DeLoge <reyna.deloge@gmail.com>
2018-12-05 13:41:28 +0100

    add input validation

--- convertToRomanNumeral.js
+++ convertToRomanNumeral.js
@@ -14,7 +14,23 @@ const romanNumeralValues = [
   { integer: 1, numeral: 'I' }
 ];
 
+const validateInput = (input) => {
+  if (typeof input !== 'number') {
+    throw new Error('Argument must be a number');
+  }
+
+  if (input <= 0 || input >= 5000) {
+    throw new Error('Argument must be between 1 and 4999');
+  }
+
+  if (!Number.isInteger(input)) {
+    throw new Error('Argument must be an integer');
+  }
+}
+
 const convertToRomanNumeral = (input) => {
+  validateInput(input);
+
   return romanNumeralValues.reduce((accumulator, { integer, numeral }) => {
     const count = Math.floor(input / integer);
     input -= integer * count;
